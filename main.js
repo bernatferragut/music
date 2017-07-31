c37c001df13bba3a4e5d2a9f1a7365880db7808c
Bernat Ferragut <elbernat@gmail.com>
2017-07-31 09:16:07 -0400

    Web API two skiers, volume and oscillator achieved

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 
+// 1. WHEN DOM READY
 $(document).ready(function() {
-  var rangeSlider = function(){
-  var slider = $('.range-slider'),
-      range = $('.range-slider__range'),
-      value = $('.range-slider__value');
-    
-  slider.each(function(){
+    // RANGESLIDER OBJECT FUNCTION
+    var rangeSlider = function(){
+    var slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
 
-    value.each(function(){
-      var value = $(this).prev().attr('value');
-      $(this).html(value);
+    // for each of the sliders apply...
+    slider.each(function(){
+      // for each of the values apply...
+      value.each(function(){
+        // get the attr 'before' value of the slide
+        var value = $(this).prev().attr('value');
+        // aply it now on the new
+        $(this).html(value);
+      });
+      // onInput()
+      range.on('input', function(){
+        $(this).next(value).html(this.value);
+      });
     });
+  };
 
-    range.on('input', function(){
-      $(this).next(value).html(this.value);
-    });
+  //RANGESLIDER CREATION
+  rangeSlider();
+
+  // WEB AUDIO API  // declare vars
+  const context = new window.AudioContext()
+  // OSCILLATOR
+  let osc = context.createOscillator();
+  let freq = osc.frequency;
+  freq.value = 440;
+  osc.type = 'sine'; //(sine, square, traingle, sawtooth)
+  let freqControl = document.getElementById('frequency');
+  // VOLUME
+  let vol = context.createGain();
+  let volControl = document.getElementById('volume');
+  // osc => vol
+  osc.connect(vol);
+  // hook up values
+  vol.gain.value = volControl.value;
+  // vol => destination
+  vol.connect(context.destination);
+  // VOLUME CONTROL
+  volControl.addEventListener('input', function() {
+    vol.gain.value = volControl.value;
   });
-};
+  // FREQUENCY CONTROL
+  freqControl.addEventListener('input', function() {
+    freq.value = freqControl.value;
+  });
+  //start osc
+  osc.start();
+  osc.stop();
 
-rangeSlider();
-})
+  // CREATING AN OBSERVABLE
+
+});
